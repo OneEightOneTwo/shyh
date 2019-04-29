8ff9ed50df4e6bbe0a8fbbc36b110ccc38dbebbf
lzhihao04 <965278809@qq.com>
2019-04-29 19:20:55 +0800

    初始布局

---
+++ dva-quickstart/src/routes/IndexPage.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'dva';
+import style from './IndexPage.css';
+
+function IndexPage() {
+
+  return (
+    <div className={style.war}>
+                    {/* 头部 、、、、、、、、、、、、、、、、、*/}
+        <div className={style.headers}>头部组件</div>
+                    {/* 内容、、、、、、、、、、、、、、、、、、 */}
+        <div className={style.container}>
+                    {/* 右侧浮动内容 */}
+          <div className={`${style.fr} ${style.w706}`}>
+                    {/* 轮播图 */}
+            <div className={style.rolling}>轮播图组件</div>
+                    {/* 四个卡片 */}
+            <div className={`${style.fl} ${style.w348}`}>卡片1，高度已经写死，需要根据内容适配来改高度</div>
+            <div className={`${style.fr} ${style.w348}`}>卡片2，高度已经写死，需要根据内容适配来改高度</div>
+            <div className={`${style.fl} ${style.w348}`}>卡片3，高度已经写死，需要根据内容适配来改高度</div>
+            <div className={`${style.fr} ${style.w348}`}>卡片4，高度已经写死，需要根据内容适配来改高度</div>
+                    {/* 选项卡 */}
+            <div className={`${style.fr} ${style.t_bar}`}>选项卡组件</div>
+          </div>
+                    {/* 左侧浮动内容 */}
+          <div className={`${style.fl} ${style.w_263}`}>
+                    {/* 个人网上银行小卡片 */}
+            <div className={style.gr_car}>个人网上银行小卡片</div>
+                    {/* 手风琴 */}
+            <div className={style.accordion}>手风琴组件</div>
+                    {/* 图片 */}
+            <div className={style.w_263}>图片</div>
+          </div>
+        </div>
+                    {/* 底部 //////////////*/}
+        <div className={`${style.footers} ${style.clear}`}>底部组件</div>
+    </div>
+  );
+}
+
+IndexPage.propTypes = {
+};
+
+export default connect()(IndexPage);
